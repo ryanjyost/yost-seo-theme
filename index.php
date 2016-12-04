c0a20402fa850e113021a271cdf2807a9b5ae1b2
Ryan Yost <ryost217@gmail.com>
2016-12-04 15:28:34 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php wp_head() ?>
+  <body>
+    <div class="container-fluid">
+
+      <h1>
+        Hello, world!
+        <span class="glyphicon glyphicon-align-left" aria-hidden="true"></span>
+
+      </h1>
+
+
+    </div>
+
+
+
+  </body>
+</html>
