6d4a2986241692159d5b1964161ad278a2560b5b
Ryan Yost <ryost217@gmail.com>
2016-12-05 11:06:43 -0700

    Coming Soon
    Just a placeholder until actual site is up

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
-<?php wp_head() ?>
+<?php
+/*******************
+Index page
+********************/
+
+?>
+
+
+<?php get_header() ?>
   <body>
-    <div class="container-fluid">
+    <div class="jumbotron">
+    	<div class="container-fluid">
 
-      <h1>
-        Hello, world!
-        <span class="glyphicon glyphicon-align-left" aria-hidden="true"></span>
+    		<h1>
+	        	Yost SEO is coming soon!
+	      	</h1>
 
-      </h1>
+	      	<h3 class="get-in-touch">
+	      		In the meantime, please feel free to reach out using the form below.
+	      	</h3>
 
+	      	<div id="contact-form">
+		      	<?php echo do_shortcode('[contact-form-7 id="5" title="Contact"]');
+		      	?>
+	      	</div>
+
+    	</div>
 
     </div>
 
